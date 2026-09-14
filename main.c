0b4ec4dcf99de0239aae6ee65d3cc95cbc05b536
Fanty1107 <edufantinlemes@gmail.com>
2026-09-14 19:18:22 -0300

    67

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main() {
   int x = 20 + 20 + 20;
-  float y = 7.67676767;
+  float y = 7.66666767;
 
   char *word = "gene";
   char *word2 = "rics";
@@ -11,7 +11,7 @@ int main() {
   Type(x, y) result = add_generics(x, y);
   Type(word, word2) result2 = add_generics(word, word2);
 
-  printf("result: %f\n", result);
+  printf("result: %.2f\n", result);
   printf("result str: %s", result2);
   return 0;
 }
